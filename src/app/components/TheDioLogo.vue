3fb23d940a9eb420caf9758842ae9a59339e295b
Adriel da Silva <silvaadriel510@gmail.com>
2019-11-21 15:55:18 -0300

    Improve dashboard

--- src/app/components/TheDioLogo.vue
+++ src/app/components/TheDioLogo.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <v-img class="mt-4" alt="Dio logo" src="../assets/dio-logo.svg"></v-img>
+    <v-img :width="width" alt="Dio logo" src="../assets/dio-logo.svg"></v-img>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TheDioLogo',
+  props: {
+    width: {
+      type: String,
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.logo {
+  height: 100%;
+}
+</style>
--- src/app/views/Dashboard.vue
+++ src/app/views/Dashboard.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Dashboard</h1>
+  <base-header>
+    <template slot="body">
+      <router-link to="/">
+        <the-dio-logo width="80"/>
+      </router-link>
+    </template>
+  </base-header>
 </template>
 
 <script>
+import BaseHeader from '../components/BaseHeader.vue';
+import TheDioLogo from '../components/TheDioLogo.vue';
+
 export default {
   name: 'Dashboard',
+  components: {
+    BaseHeader,
+    TheDioLogo,
+  },
 };
 </script>
