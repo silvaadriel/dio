093fe32723700618b4fbec6cd478f11b7f224998
Adriel da Silva <silvaadriel510@gmail.com>
2019-11-29 15:15:11 -0300

    Improve chart

--- src/app/components/DataTable.vue
+++ src/app/components/DataTable.vue
@@ -1,15 +1,17 @@
 <template>
   <v-card>
     <v-card-title>
-      {{ data.title }}
-      <v-spacer></v-spacer>
       <v-text-field
         v-model="search"
-        append-icon="search"
+        prepend-icon="search"
         label="Search"
         single-line
         hide-details
+        clearable
       ></v-text-field>
+      <v-spacer></v-spacer>
+      <h2 class="mr-2 grey--text text--darken-2">Data Table</h2>
+      <v-icon color="grey darken-2" size="54">list_alt</v-icon>
     </v-card-title>
     <v-data-table
       calculate-widths
@@ -21,7 +23,7 @@
 </template>
 
 <script>
-import DataModel from '../models/DataModel';
+// import DataModel from '../models/DataModel';
 
 export default {
   name: 'DataTable',
@@ -30,7 +32,7 @@ export default {
   }),
   props: {
     data: {
-      type: DataModel,
+      type: Object,
       required: true,
     },
   },
--- src/app/views/Dashboard.vue
+++ src/app/views/Dashboard.vue
@@ -1,24 +1,39 @@
 <template>
-  <v-container text-center fill-height>
-    <v-layout column align-center justify-start>
-      <base-header class="mb-4">
-        <template slot="body">
-          <router-link to="/">
-            <the-dio-logo width="80"/>
-          </router-link>
-        </template>
-      </base-header>
-      <base-chart class="mb-4"/>
+  <div>
+    <v-container class="header" text-center fill-height>
+      <v-layout column align-center justify-center>
+        <base-header>
+          <template slot="body">
+            <router-link to="/">
+              <the-dio-logo width="80"/>
+            </router-link>
+          </template>
+        </base-header>
+        <h1 class="grey--text text--darken-2 font-weight-black">{{fileName}}</h1>
+        <v-btn
+          icon
+          x-large
+          absolute
+          bottom
+          color="grey darken-2"
+          @click="$vuetify.goTo('#scroll-target')"
+        >
+          <v-icon size="44">arrow_downward</v-icon>
+        </v-btn>
+      </v-layout>
+    </v-container>
+    <v-container text-center id="scroll-target">
+      <!-- <base-chart class="mb-8"/> -->
       <data-table :data="csv"/>
-    </v-layout>
-  </v-container>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import BaseHeader from '../components/BaseHeader.vue';
 import TheDioLogo from '../components/TheDioLogo.vue';
 import DataTable from '../components/DataTable.vue';
-import BaseChart from '../components/BaseChart.vue';
+// import BaseChart from '../components/BaseChart.vue';
 
 export default {
   name: 'Dashboard',
@@ -27,12 +42,22 @@ export default {
     csv() {
       return this.getCsv();
     },
+    fileName() {
+      return this.$route.query.fn;
+    },
   },
   components: {
     BaseHeader,
     TheDioLogo,
     DataTable,
-    BaseChart,
+    // BaseChart,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.header {
+  position: relative;
+  height: 100vh;
+}
+</style>
